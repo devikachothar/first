048f49a1954746d18034ff97e8b1459b214b6100
devikachothar <devikachothar@gmail.com>
2017-03-17 22:21:30 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -3,11 +3,13 @@ void main()
 {
  int year;
  scanf("%d",&year);
- while(year>4)
+ if(year>=4)
  {
  if(year%4==0)
    printf("Leap year");
    else
    printf("Not leap year");
  }  
+ else
+  printf("Wrong input");
  }
